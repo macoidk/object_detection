c48635e3204fe6bef36877f55e38e20f665112d8
Kostiantyn <fightm45@gmail.com>
2025-01-12 11:32:25 +0200

    modify

---
+++ export_model_onxx/export_model_onxx.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.path.append('..')
+
+from models.centernet import ModelBuilder, IMG_HEIGHT, IMG_WIDTH
+from utils.convert_to_onxx import export_to_onnx
+
+
+def main():
+    # Конфігурація моделі
+    backbone_name = "resnet50"
+    weights = 'DEFAULT'
+
+    # Створення моделі
+    model = ModelBuilder(
+        alpha=1,
+        backbone=backbone_name,
+        backbone_weights=weights,
+    )
+
+    # Експорт моделі
+    export_to_onnx(
+        model=model,
+        img_height=IMG_HEIGHT,
+        img_width=IMG_WIDTH,
+        save_path=f"onnx/model_{backbone_name}",
+        batch_size=1,
+        show_summary=True,
+        test_model=True
+    )
+
+
+if __name__ == "__main__":
+    main()
